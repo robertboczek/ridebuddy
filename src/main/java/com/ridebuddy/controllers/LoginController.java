c3dd11107cf4fdba13a4dc1e0e3a967db9004ee5
Robert Boczek <robert.boczek@gmail.com>
2014-01-25 14:03:09 -0800

    bug fix

--- src/main/java/com/ridebuddy/controllers/LoginController.java
+++ src/main/java/com/ridebuddy/controllers/LoginController.java
@@ -53,7 +53,7 @@ public class LoginController extends AbstractController {
 		logger.info("Received FB login: " + fbLogin);
 		FbUser fbUser = null;
 		try {
-			fbUser = getFbUser("https://graph.facebook.com/oauth/access_token?client_id=204946226368181&redirect_uri=http://ec2-50-16-158-177.compute-1.amazonaws.com:8080/whereismymobile/fbLogin&client_secret=961f70d8402f92d77b492d74e5408a&code="
+			fbUser = getFbUser("https://graph.facebook.com/oauth/access_token?client_id=204946226368181&redirect_uri=http://ec2-50-16-158-177.compute-1.amazonaws.com:8080/ridebuddy/fbLogin&client_secret=961f70d8402f92d77b492d74e5408a&code="
 					+ fbLogin.getCode());
 		} catch (Exception e) {
 			logger.error("Error while getting fb token", e);
